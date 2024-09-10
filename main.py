f7f2cd6d3ef5d3868bb5249fa7c3a29a5b1da3a4
Gustavo Santacruz <gustavosantacruz17@hotmail.com>
2024-09-09 21:12:05 -0500

    agregamos mas peliculas

--- main.py
+++ main.py
@@ -19,7 +19,56 @@ movies_list = [
         "overview": "PEZ PERDIDO",
         "year": 2010,
         "rating": 10.0
-    }
+    },
+    {
+        "id": 3, 
+        "title": "AVATAR",
+        "overview": "Ciencia ficción épica",
+        "year": 2009,
+        "rating": 10.0},
+    {
+        "id": 4, 
+        "title": "TITANIC",
+        "overview": "Romance y desastre",
+        "year": 1997,
+        "rating": 10.0},
+    {
+        "id": 5, 
+        "title": "Shrek 2",
+        "overview": "Comedia animada",
+        "year": 2004,
+        "rating": 10.0},
+    {
+        "id": 6, 
+        "title": "Harry Potter y la piedra filosofal",
+        "overview": "Fantasía mágica ",
+        "year": 2001,
+        "rating": 10.0},
+    {
+        "id": 7, 
+        "title": "El Señor de los Anillos: El Retorno del Rey",
+        "overview": "Fantasía épica",
+        "year": 2003,
+        "rating": 10.0},
+    {
+        "id": 8, 
+        "title": "La Era de Hielo 2",
+        "overview": "Animación y comedia",
+        "year": 2002,
+        "rating": 10.0},
+    {
+        "id": 9, 
+        "title": "Piratas del Caribe: El Cofre de la Muerte",
+        "overview": "Superhéroes y acción",
+        "year": 2002,
+        "rating": 10.0},
+    {
+        "id": 10, 
+        "title": "Piratas del Caribe: El Cofre de la Muerte",
+        "overview": "Aventura y fantasía",
+        "year": 2007,
+        "rating": 10.0},
+        
 ]
 @app.get('/', tags=["Home"])
 def message():
